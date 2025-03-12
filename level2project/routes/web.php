4765eb41d35a213c9f853845616e0b235680adc1
Hamza Zaitoun <hamzasaadzaitoun@gmail.com>
2025-03-12 16:23:56 +0300

    adding auth

--- level2project/routes/web.php
+++ level2project/routes/web.php
@@ -38,27 +38,38 @@ Route::name('front.')->group(function () {
     })->name('contact');    
 
 });
+
+
 Route::name('admin.')->prefix('admin')->group(function () {
-    Route::get('/', function () {
-        return view('admin.index');
-    })->name('index');
+    
+    
+    Route::middleware('auth')->group(function (): void
+     {
+        
+        Route::get('/', function ()
+         {
+            return view('admin.index');
+        })->name('index');
+        
+    });
+    require __DIR__.'/auth.php';
 });
 
 
 
 
-Route::get('/', function () {
-    return view('front.index');
-});
+// Route::get('/', function () {
+//     return view('front.index');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
-require __DIR__.'/auth.php';
+
